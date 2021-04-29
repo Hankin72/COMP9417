feeda33a9e0398cdabddb62af139bb2bd81de93e
Hankin72 <tracyguoji@qq.com>
2021-04-29 23:45:58 +0800

    unsw comp9417 21t1

--- practice.py
+++ practice.py
@@ -1,34 +1,30 @@
-import time
-
-from ipywidgets import Controller, Button
+# from ipywidgets import Controller, Button
 from pynput.keyboard import Controller as key_cl
-from pynput.mouse import Controller,Button
+from pynput.mouse import Controller, Button
 import time
 
 
 def keyborad_input(string):
-
     '''
     :params striing: hello
     :return: None
     '''
-    keyboard = key_cl()   #开始控制键盘
+    keyboard = key_cl()  # 开始控制键盘
     keyboard.type(string)  # 键盘输入string
 
-def mouse_click(): #  点击发送信息
-    mouse = Controller()      # 开始控制鼠标
-    mouse.press(Button.left)     # 按住鼠标左键
-    mouse.release(Button.left)    # 放开鼠标左键
 
+def mouse_click():  # 点击发送信息
+    mouse = Controller()  # 开始控制鼠标
+    mouse.press(Button.left)  # 按住鼠标左键
+    mouse.release(Button.left)  # 放开鼠标左键
 
 
 def main(number, string):
-    time.sleep(5)   #  暂停五秒
+    time.sleep(5)  # 暂停五秒
     for i in range(number):
         keyborad_input(string + str(i))
         mouse_click()
         time.sleep(0.1)
-
 
 
 if __name__ == "__main__":
